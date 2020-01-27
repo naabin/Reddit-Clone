9962ffe6e597567be3fecb00f07fecda8506c068
Nabin Karki <naabin@outlook.com>
2020-01-27 14:01:46 +1100

    Added mappings form link->coment

--- src/main/java/com/redditclone/domain/Comment.java
+++ src/main/java/com/redditclone/domain/Comment.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import io.micrometer.core.lang.NonNull;
 import lombok.Data;
@@ -21,4 +22,6 @@ public class Comment {
     @NonNull
     private String body;
 
+    @ManyToOne
+    private Link link;
 }
--- src/main/java/com/redditclone/domain/Link.java
+++ src/main/java/com/redditclone/domain/Link.java
@@ -1,8 +1,12 @@
 package com.redditclone.domain;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,4 +26,7 @@ public class Link {
 
     @NonNull
     private String url;
+
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
 }
