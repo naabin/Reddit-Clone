64f201c9380981ca4be038fb2805fc0253c07f92
Nabin Karki <naabin@outlook.com>
2020-01-27 12:50:24 +1100

    Added entities and refactored with Project lombok

--- src/main/java/com/redditclone/RedditcloneApplication.java
+++ src/main/java/com/redditclone/RedditcloneApplication.java
@@ -1,15 +1,40 @@
 package com.redditclone;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedditcloneApplication {
+
+	@Autowired
+	ApplicationContext applicationContext;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedditcloneApplication.class, args);
 
 		System.out.println("Welcome to Reddit clone using spring boot");
+
+
+
+	}
+
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+			System.out.println("Printing all the bean names in the application context.");
+			System.out.println("------------------------------------------------------");
+			String [] beans = this.applicationContext.getBeanDefinitionNames();
+			Arrays.sort(beans);
+			for(String bean: beans){
+				System.out.println(bean);
+			}
+		};
 	}
 
 }
---
+++ src/main/java/com/redditclone/domain/Vote.java
@@ -0,0 +1,23 @@
+package com.redditclone.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import io.micrometer.core.lang.NonNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Vote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private int vote;
+}
